fa8ffce8a4722cd4b49d6005d0aa4298a599d6a5
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-18 22:29:19 -0300

    Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import styled, { keyframes } from "styled-components";
+
+const Layout = styled.div`
+  height: 100vh;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+export default Layout;
